5b5598eeaa4f0e36c3ba368aa38d29da7e7df9b9
Петр Иванов <petara94@mail.ru>
2020-10-26 16:16:49 +0300

    linux-windows

--- client.cpp
+++ client.cpp
@@ -1,7 +1,14 @@
+
+#ifdef __linux__
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#elif _WIN32
+#include <winsock2.h>
+#include < ws2tcpip.h >
+#endif
+
 #include <iostream>
 #include <string.h>
 using namespace std;
--- server.cpp
+++ server.cpp
@@ -1,7 +1,12 @@
+#ifdef __linux__
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#elif _WIN32
+#include <winsock2.h>
+#include < ws2tcpip.h >
+#endif
 #include <boost/algorithm/string.hpp>
 #include <iostream>
 #include <vector>
